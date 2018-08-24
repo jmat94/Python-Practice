b568bfec77d99c42837d1d4674bfe05009e0fa6f
Jordan Mathew <rooneymathew94@gmail.com>
2018-08-24 16:27:00 +0530

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -1,15 +1,19 @@
 import time
 
+# Creating the bank account class.
 class BankAccount():
 
+    # Calling the __init__ constructor and assigning attributes to variables
     def __init__(self, owner, balance):
         self.owner = owner
         self.balance = balance
-
+    
+    # Method to provide account details
     def acc_info(self):
         print('Account owner: {}'.format(self.owner))
         print('Account balance: {}'.format(self.balance))
-
+        
+    # Method to deposit a particular amount
     def deposit(self, amount_to_deposit):
         self.amount_to_deposit = amount_to_deposit
 
@@ -17,7 +21,9 @@ class BankAccount():
         print('You have added {} to your account.'.format(amount_to_deposit))
 
         self.balance = self.balance + amount_to_deposit
-
+        
+      
+    # Method to withdra from whatever funds are available
     def withdraw(self, amount_to_withdraw):
 
 
@@ -30,7 +36,7 @@ class BankAccount():
             print('you can withdraw from {}'.format(self.balance))
 
 
-
+# Object creation
 acct1 = BankAccount('Jose', 100)
 acct2 = BankAccount('Jordan', 150)
 
